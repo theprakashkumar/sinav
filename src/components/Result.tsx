2f74853381711166c8ba868b8d32dbab26fb8362
PRAKASH <38276550+ThePrakashKumar@users.noreply.github.com>
2021-07-26 14:06:52 +0530

    feat: added more element to result page

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,12 +1,30 @@
+import "./Result.css";
 import { useContext } from "react";
 import { DataContext } from "../contexts/DataContext";
+import { IndexKind } from "typescript";
 const Result = () => {
-    const { score, selectedAnswer } = useContext(DataContext)
-    console.log(selectedAnswer)
-    return(
+    const { score, quiz, selectedAnswer } = useContext(DataContext);
+    console.log(selectedAnswer);
+    return (
         <div className="Result">
-             <div>Your Final Score: {score}</div>
-             {selectedAnswer.map((item, index)=> <p key={index}>{item}</p>)}
+            <div>Your Final Score: {score}</div>
+            {quiz.questions.map((question, index) => (
+                <>
+                    <p>{question.question}</p>
+                    {question.options.map((option) => (
+                        <div
+                            className={`op ${option.isRight && "right"}  ${
+                                selectedAnswer[index] == option.id && !option.isRight && "wrong"
+                            }`}
+                        >
+                            {` ${index}. ${option.text}`}
+                        </div>
+                    ))}
+                </>
+            ))}
+            {selectedAnswer.map((item, index) => (
+                <p key={index}>{item}</p>
+            ))}
         </div>
     );
 };
